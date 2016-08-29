f0f0331bae9c5b64c216f8a71794000bf9ba619c
Dmitry Kunin <dkunin@avito.ru>
2016-08-29 13:21:37 +0300

    master:Added pincode for pheonix, updated week to show previous links

--- bitplugins/week.1d.sh
+++ bitplugins/week.1d.sh
@@ -7,9 +7,15 @@
 #
 # Displays weeknumber
 DATE=W$(date +%V)
+PDATE=W$(date +%V | awk '{print $1 - 1}')
 echo $DATE
 echo "---"
-echo "Open Advert | color=gray href=https://cf.avito.ru/display/GE/$DATE%3A+Advert"
-echo "Open Billing | color=gray href=https://cf.avito.ru/display/GE/$DATE%3A+Billing"
-echo "Open CRM | color=gray href=https://cf.avito.ru/display/GE/$DATE%3A+CRM"
-echo "Open Week | color=gray href=https://cf.avito.ru/display/GE/$DATE"
+echo "Open Advert | href=https://cf.avito.ru/display/GE/$DATE%3A+Advert"
+echo "Open Billing | href=https://cf.avito.ru/display/GE/$DATE%3A+Billing"
+echo "Open CRM | href=https://cf.avito.ru/display/GE/$DATE%3A+CRM"
+echo "Open Week | href=https://cf.avito.ru/display/GE/$DATE"
+echo "$PDATE :"
+echo "Open Advert ($PDATE)| color=gray href=https://cf.avito.ru/display/GE/$PDATE%3A+Advert"
+echo "Open Billing ($PDATE)| color=gray href=https://cf.avito.ru/display/GE/$PDATE%3A+Billing"
+echo "Open CRM ($PDATE)| color=gray href=https://cf.avito.ru/display/GE/$PDATE%3A+CRM"
+echo "Open Week ($PDATE)| color=gray href=https://cf.avito.ru/display/GE/$PDATE"
