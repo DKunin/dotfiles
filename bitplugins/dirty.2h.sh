e22834f9ba0492ebef5fd815a12a7b1c126c2e9f
Dmitry Kunin <dkunin@avito.ru>
2016-08-31 09:54:57 +0300

    master:Added dirty to checkout

---
+++ bitplugins/dirty.2h.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+# Get dirty projects (not staged, or non commited)
+# BitBar plugin
+#
+# by Dkunin
+
+MAIN_PROJECTS=$(/Users/dikunin/bin/dirty /Users/dikunin/Projects | wc -l | tr -d '[[:space:]]')
+SUB_PROJECTS=$(/Users/dikunin/bin/dirty /Users/dikunin/SubProject | wc -l | tr -d '[[:space:]]')
+echo $MAIN_PROJECTS/$SUB_PROJECTS
