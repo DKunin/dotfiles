c1be84ff0f2654a689eb3c61a410296ab6f8bc08
Dmitry Kunin <dkunin@avito.ru>
2016-09-16 13:11:51 +0300

    master:Added link to seneca letter

--- bitplugins/days.1d.sh
+++ bitplugins/days.1d.sh
@@ -4,9 +4,12 @@
 // BitBar plugin
 // by Dkunin
 // Display number of days, since you were born
-
+//65
 var seconds = (new Date() - new Date('01.14.1985')) / 1000;
 var minutes = seconds / 60;
 var hours = minutes / 60;
 var days = Math.ceil(hours / 24);
 console.log(days);
+console.log('---');
+var letterNumber = days - 11569 + 65;
+console.log('Seneca Letter | href=http://dkunin.github.io/seneca-letters/html/txt0' + letterNumber + '.html');
