581d70f0caaa9adb6d04e052eef455fe6021dfdf
Dmitry Kunin <dkunin@avito.ru>
2017-02-01 14:07:50 +0300

    master:Minor modifications to user script

--- bitplugins/user.30m.sh
+++ bitplugins/user.30m.sh
@@ -6,9 +6,8 @@
 # by Dkunin
 #
 # Displays time of waka
-echo -n white | nc -4u -w0 localhost 1738
-NODE=/Users/dikunin/.nvm/versions/node/v4.2.3/bin/node
-WAKACLI=/Users/dikunin/.nvm/versions/node/v4.2.3/lib/node_modules/wakacli
+NODE=/Users/dikunin/.nvm/versions/node/v7.0.0/bin/node
+WAKACLI=/Users/dikunin/.nvm/versions/node/v7.0.0/lib/node_modules/wakacli
 TIMER=$($NODE $WAKACLI --nospinner)
 TIMERCUT=${TIMER:8}
 echo $TIMERCUT
