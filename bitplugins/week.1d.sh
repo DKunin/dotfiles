57fae13e8ff9f706d3d64758d587be62419ee726
Dmitry Kunin <dkunin@avito.ru>
2017-02-06 16:14:56 +0300

    master:Added plans for today to bitplugin

--- bitplugins/week.1d.sh
+++ bitplugins/week.1d.sh
@@ -6,18 +6,10 @@
 # by Dkunin
 #
 # Displays weeknumber
+PLAN=$(/Users/dikunin/Projects/work-calendar-exchange/calendar)
 DISPLAY_DATE=$(date +%d\ %b\ \(W%V\))
-DATE=W$(date +%V)
-PDATE=W$(date +%V | awk '{print $1 - 1}')
+# DATE=W$(date +%V)
+# PDATE=W$(date +%V | awk '{print $1 - 1}')
 echo $DISPLAY_DATE
 echo "---"
-echo "$DATE :"
-echo "Open Advert | href=https://cf.avito.ru/display/GE/$DATE%3A+Advert"
-echo "Open Billing | href=https://cf.avito.ru/display/GE/$DATE%3A+Billing"
-echo "Open CRM | href=https://cf.avito.ru/display/GE/$DATE%3A+CRM"
-echo "Open Week | href=https://cf.avito.ru/display/GE/$DATE"
-echo "$PDATE :"
-echo "Open Advert ($PDATE) | color=gray href=https://cf.avito.ru/display/GE/$PDATE%3A+Advert"
-echo "Open Billing ($PDATE) | color=gray href=https://cf.avito.ru/display/GE/$PDATE%3A+Billing"
-echo "Open CRM ($PDATE) | color=gray href=https://cf.avito.ru/display/GE/$PDATE%3A+CRM"
-echo "Open Week ($PDATE) | color=gray href=https://cf.avito.ru/display/GE/$PDATE"
+echo $PLAN
