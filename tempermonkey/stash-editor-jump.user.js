c0c8398b0fd3a7781106154ab3fe718f1c659d6f
Dmitry Kunin <dkunin@avito.ru>
2017-04-25 19:09:02 +0300

    master:Added stash jump script

---
+++ tempermonkey/stash-editor-jump.user.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name         StashEditorJump
+// @namespace    http://stash.msk.avito.ru
+// @version      0.7
+// @description  Add ability to link to file position from PR.
+// @author       DKunin
+// @include      http://stash.msk.avito.ru/projects/AV/repos/avito-site/pull-requests/*
+// @include      http://stash.msk.avito.ru/projects/AV/repos/avito-site/commits/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const STORAGE_KEY = 'stash-jumper-projects';
+    const storage = JSON.parse(
+        localStorage.getItem(STORAGE_KEY)
+    ) || {};
+    const pathName = location.pathname.split('/');
+    const projectHash = pathName[2] + pathName[4];
+    let projectHome = storage[projectHash] || '';
+
+    const subMenu = document.querySelector('.pull-request-metadata');
+
+    var divider = document.createElement('div');
+    divider.classList.add('divider');
+    divider.style = 'margin-left: 10px';
+    var projectPathInput = document.createElement('input');
+    projectPathInput.value = projectHome;
+    projectPathInput.title = projectHome;
+    projectPathInput.addEventListener('change', event => {
+        projectHome = event.target.value;
+        projectPathInput.title = projectHome;
+        storage[projectHash] = projectHome;
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(storage));
+    });
+
+    subMenu.appendChild(divider);
+    subMenu.appendChild(projectPathInput);
+
+    document.addEventListener('click', function(event) {
+        if (
+            event.target.classList.toString() ===
+            'line-number line-number-to bitbucket-gutter-marker'
+        ) {
+            var file = event.target.closest('.file-content');
+            var lineNumber = file.querySelector(
+                '.comment-container.comment-box'
+            );
+            if (lineNumber) {
+                lineNumber = lineNumber.dataset.lineNumber;
+            } else {
+                lineNumber = event.target.innerText;
+            }
+            var path = file.querySelector('.breadcrumbs').innerText;
+            new Image().src =
+                'http://localhost:7288/openeditor?options="' +
+                projectHome +
+                path +
+                ':' +
+                lineNumber +
+                '"';
+        }
+    });
+})();
