50a7c445375913d2bd7e26a997a01b374869353e
Dmitry Kunin <dkunin@avito.ru>
2017-09-04 12:28:12 +0300

    Mousetrapupdate

--- mouse-trap-todoist-setting.js
+++ mouse-trap-todoist-setting.js
@@ -1,14 +1,11 @@
-Mousetrap.bind('i n', function() { 
-  document.querySelectorAll('.filter')[0].click()
+Mousetrap.bind('i n', function() {
+    document.querySelectorAll('.filter')[0].click();
 });
 
-Mousetrap.bind('t d', function() { 
-  document.querySelectorAll('.filter')[2].click()
+Mousetrap.bind('t d', function() {
+    document.querySelectorAll('.filter')[2].click();
 });
 
-Mousetrap.bind('t m', function() { 
-    some === asd
-    ==> 
-    
-  document.querySelectorAll('.filter')[3].click()
+Mousetrap.bind('t m', function() {
+    document.querySelectorAll('.filter')[3].click();
 });
