3d9a0a83f3b09bdcd90e7190bd94f4a2d433d638
Dmitry Kunin <dkunin@avito.ru>
2017-10-17 09:34:23 +0300

    Update of mouse script

--- mouse-trap-todoist-setting.js
+++ mouse-trap-todoist-setting.js
@@ -9,3 +9,14 @@ Mousetrap.bind('t d', function() {
 Mousetrap.bind('t m', function() {
     document.querySelectorAll('.filter')[3].click();
 });
+
+Mousetrap.bind('a t', function() {
+    const AvitoToday = Array.from(
+        document.querySelectorAll('#filters_list li')
+    ).find(function(singleItem) {
+        return singleItem.innerText.trim() === 'Avito Today';
+    });
+    if (AvitoToday) {
+        AvitoToday.click();
+    }
+});
