6b7826af09226881535f14f444a628f314a5d187
Dmitriy Kunin <dkunin@avito.ru>
2023-02-17 08:25:39 +0300

    Update

--- bitplugins/goals.1d.sh
+++ bitplugins/goals.1d.sh
@@ -7,31 +7,10 @@
 #
 # Displays weekly Goals
 
-goals=( "✅ Запустить обновление версии" "✅  Запустить маркетинг про обновление")
-nextgoals=( "Раскатить жесткое обновление" "Раскатить АБ мессенджера на 100%" "Запустить АБ поиска и рекомендацишй")
-len=${#goals[@]}
-
-echo "Цели 2/${len}";
+echo "Цели";
 echo "---";
-for i in "${goals[@]}"
-do
-	echo "$i"
-done
-# echo "---";
-# echo "";
-# echo "*Сегодня:*";
-# echo "Встречи:";
-# echo "";
-# # $PROJECTS/work-calendar-exchange/calendar;
-# echo "";
-# echo "Задачи:";
-# echo "";
-# whats-my-tech-lead-up-to;
-
+/Users/dkunin/Projects/bin/weekgoals
 echo "---";
-for i in "${nextgoals[@]}"
-do
-	echo "$i"
-done
+/Users/dkunin/Projects/bin/work-calendar
 
 
