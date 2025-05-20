15f93ff2311223165417010e41de26c7770dc873
Dmitriy Kunin <dkunin@avito.ru>
2025-05-20 10:02:44 +0300

    Update

--- bash_profile
+++ bash_profile
@@ -47,3 +47,5 @@ unset __conda_setup
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+. "$HOME/.local/bin/env"
---
+++ finicky.js
@@ -0,0 +1,90 @@
+// Use https://finicky-kickstart.now.sh to generate basic configuration
+// Learn more about configuration options: https://github.com/johnste/finicky/wiki/Configuration
+
+const BoardMixApp = {
+  browser: ({ urlString }) => ({
+    name: "Google Chrome",
+    // profile: "Profile 13", // use whatever profile created the application shortcut
+    args: [
+      `--app-id=jeleglcnfdjhgfelgbmeplgiegkbkbjp`, // my app ID for my Google Meet application shortcut
+      `--app-launch-url-for-shortcuts-menu-item=${urlString}`,
+      // notice I'm not passing urlString as an array entry, since Chrome Application Shortcuts don't like that
+    ],
+  }),
+};
+
+module.exports = {
+  defaultBrowser: "Google Chrome",
+  options: {
+    hideIcon: false,
+    checkForUpdate: true,
+    logRequests: true,
+  },
+  rewrite: [
+    {
+      // Redirect all urls to use https
+      match: finicky.matchDomains(/.*\.ktalk.ru/),
+      url: ({ url }) => {
+        return "http://127.0.0.1:44258/ktalk/app/open-url?url=" + url;
+      },
+    },
+
+    {
+      match: /vk\.com\/away.php/,
+      url: ({ url }) => {
+        const match = url.search.match(/to=(.+)/);
+        return !match ? url : decodeURIComponent(decodeURIComponent(match[1]));
+      },
+    },
+    {
+      match: () => true, // Execute rewrite on all incoming urls to make this example easier to understand
+      url: ({ url }) => {
+        const removeKeysStartingWith = ["utm_", "uta_"]; // Remove all query parameters beginning with these strings
+        const removeKeys = ["fbclid", "gclid"]; // Remove all query parameters matching these keys
+
+        const search = url.search
+          .split("&")
+          .map((parameter) => parameter.split("="))
+          .filter(
+            ([key]) =>
+              !removeKeysStartingWith.some((startingWith) =>
+                key.startsWith(startingWith)
+              )
+          )
+          .filter(
+            ([key]) => !removeKeys.some((removeKey) => key === removeKey)
+          );
+
+        return {
+          ...url,
+          search: search.map((parameter) => parameter.join("=")).join("&"),
+        };
+      },
+    },
+    {
+      match: finicky.matchDomains(["google.com"]),
+      url: "https://duckduckgo.com",
+    },
+  ],
+  handlers: [
+    {
+      match: [
+        finicky.matchDomains(/gitlab.senseai.pro/g),
+        finicky.matchDomains(/console.yandex.cloud/g),
+        finicky.matchDomains(/bot.senseai.pro/g),
+      ],
+      // Forward to Brave
+      browser: "Brave Browser",
+    },
+  ],
+  // handlers: [
+  //   {
+  //     match: [
+  //       // finicky.matchDomains(/boardmix.com/g) // use helper function to match on domain only
+  //     ],
+  //     // browser: "/Applications/Chrome\\ Apps.localized/BoardMix.app"
+  //     // browser: "Chrome"
+  //     ...BoardMixApp,
+  //   }
+  // ]
+};
